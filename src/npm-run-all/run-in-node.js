1ca4a7703ce20919d83adb6192e6640afb14f588
afeiship <1290657123@qq.com>
2019-06-23 21:11:26 +0800

    add: run-in-node

--- src/npm-run-all/run-in-node.js
+++ src/npm-run-all/run-in-node.js
@@ -1,17 +1,20 @@
 const runAll = require('npm-run-all');
 
-runAll(['clean', 'lint', 'build:*'], { parallel: false })
-  .then(() => {
-    console.log('done!');
+runAll(['clean', 'lint', 'build:*'], { parallel: true })
+  .then((results) => {
+    console.log('results:->', results);
+    console.log(`${results[0].name}: ${results[0].code}`); // clean: 0
+    console.log(`${results[1].name}: ${results[1].code}`); // lint: 0
+    console.log(`${results[2].name}: ${results[2].code}`); // build: 0
   })
   .catch((err) => {
     console.log('failed!');
   });
 
-runAll(['build:* -- --watch'], { parallel: true })
-  .then(() => {
-    console.log('done!');
-  })
-  .catch((err) => {
-    console.log('failed!');
-  });
+// runAll(['build:* -- --watch'], { parallel: true })
+//   .then(() => {
+//     console.log('done!');
+//   })
+//   .catch((err) => {
+//     console.log('failed!');
+//   });
